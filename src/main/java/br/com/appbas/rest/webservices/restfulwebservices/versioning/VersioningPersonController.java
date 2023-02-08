c20ede7b744ef1ca10e8d5eefff234efa1827966
Bruno Silva <bruno.sidf@gmail.com>
2023-02-08 20:41:00 -0300

    Reproduzir 38. Step 21 - Versioning REST API - Request Param, Header and Content Negotiation

--- src/main/java/br/com/appbas/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/br/com/appbas/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+	// URI Versioning - Twitter
 	@GetMapping("/v1/person")
 	public PersonV1 getFirstVersionOfPerson() {
 		return new PersonV1("Bruno Silva");
@@ -13,6 +14,39 @@ public class VersioningPersonController {
 	
 	@GetMapping("/v2/person")
 	public PersonV2 getSecondVersionOfPerson() {
+		return new PersonV2(new Name("Bruno", "Silva"));
+	}
+	
+	// Request Parameter versioning - Amazon
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Bruno Silva");
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("Bruno", "Silva"));
+	}
+	
+	// (Custom) headers versioning - Microsoft
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bruno Silva");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Bruno", "Silva"));
+	}
+	
+	// Media Type versioning
+	@GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonRequestProduces() {
+		return new PersonV1("Bruno Silva");
+	}
+	
+	@GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonRequestProduce() {
 		return new PersonV2(new Name("Bruno", "Silva"));
 	}
 	
